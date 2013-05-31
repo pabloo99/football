a03da4c469280d1621118312f5ac241c7b86deb0
pabloo99 <pawel-mazur2@o2.pl>
2013-05-31 08:30:05 +0200

    FootballBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Gajdaw\AngazeBundle\GajdawAngazeBundle(),
+            new Gajdaw\FootballBundle\GajdawFootballBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
